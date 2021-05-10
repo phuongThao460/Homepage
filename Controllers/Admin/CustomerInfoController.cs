75a03a0756650767bd3d98b05e09a63b4ff36085
kimtien123 <huynhthikimtien123@gmail.com>
2021-05-11 00:37:29 +0700

    Vài thứ gì đấy
    Cái title bên trên bắt buộc có nha ^^

--- Controllers/Admin/CustomerInfoController.cs
+++ Controllers/Admin/CustomerInfoController.cs
@@ -9,6 +9,8 @@ namespace Homepage.Controllers.Admin
 {
     public class CustomerInfoController : Controller
     {
+        // Ví dụ tui thêm dòng này
+        // Xong save
         BOOKSHOPEntities db = new BOOKSHOPEntities();
         // GET: CustomerInfo
         public ActionResult Index()
